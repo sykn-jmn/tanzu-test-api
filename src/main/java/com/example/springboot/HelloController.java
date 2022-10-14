50b1cb8bc2bf31b6a373b9cbb86ad817c5de2495
Jeman Mama <jemanmama@gmail.com>
2022-10-14 09:02:44 +0800

    Jeman Gwapo

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -1,5 +1,6 @@
 package com.example.springboot;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -11,4 +12,9 @@ public class HelloController {
 		return "Greetings from Spring Boot + Tanzu!";
 	}
 
+
+	@RequestMapping("/jeman")
+	public String jeman(){
+		return "Gwapo";
+	}
 }
